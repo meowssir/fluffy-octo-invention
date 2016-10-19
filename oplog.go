a5c8419c4123041f740f5b9a29176ad0debe02b6
Byron Grogan <byron4628@gmail.com>
2016-10-20 00:11:14 +0100

    make it better

---
+++ oplog.go
@@ -0,0 +1,81 @@
+package oplog
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"golang.org/x/net/websocket"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	origin := "http://localhost/"
+	server := "ws://localhost:12345/ws"
+)
+
+type Result struct {
+	Id bson.ObjectId `bson:"_id,omitempty"`
+}
+
+// This struct represents a BSON document containing the elements of an oplog entry.
+type OplogEntry struct {
+	Ts time.Time
+	T  int64
+	H  int64
+	V  int64
+	Op string
+	Ns string
+	O  Result
+}
+
+// FIXME: unmarshal getter/setter.
+func websocketDial(o Result) {
+	config, err := websocket.NewConfig(server, origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// TODO: Evaluate handshake latency overhead with the additional 9 bytes of header data.
+	config.Header.Add("Mongo", "true")
+	ws, err := websocket.DialConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ws.Write([]byte(o))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func tailOplog() {
+	_ = "breakpoint"
+	session, err := mgo.Dial(url)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	result := OplogEntry{}
+	lastId := Result{}
+
+	collection := session.DB("local").C("oplog.rs")
+	iter := collection.Find(bson.M{"ns": "test.foo"}).Sort("$natural").Tail(5 * time.Second)
+	for {
+		for iter.Next(&result) {
+			fmt.Println(result.O)
+			lastId = result.O
+			// TODO: connect and send an ObjectId. GetBSON() to unmarshal and decode to bytes.
+			websocketDial(lastId)
+		}
+		if iter.Err() != nil {
+			fmt.Println("an error happened")
+		}
+		if iter.Timeout() {
+			continue
+		}
+		query := collection.Find(bson.M{"_id": bson.M{"$gt": lastId}})
+		iter = query.Sort("$natural").Tail(5 * time.Second)
+	}
+	iter.Close()
+}
---
+++ tail.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	clients   map[*Client]string
+	broadcast chan []byte
+	register  chan *Client
+}
+
+func newServer() *Server {
+	return &Server{
+		broadcast: make(chan []byte),
+		register:  make(chan *Client),
+		clients:   make(map[*Client]string),
+	}
+}
+
+type Client struct {
+	id     string
+	server *Server
+	conn   *websocket.Conn
+	send   chan []byte
+	msg    []byte
+}
+
+func randId() string {
+	// NOTE: if setting an identifying header upon Dial then this may be redundant.
+	rand.Seed(time.Now().UnixNano())
+	var runes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, 8)
+	for i := range b {
+		b[i] = runes[rand.Intn(len(runes))]
+	}
+	return string(b)
+}
+
+func (c *Client) readPump() {
+	// NOTE: Pump messages from *websocket.Conn to *Server. writePump does the reverse.
+	// TODO: defer connection.
+	msg := []byte{}
+	for {
+		err := websocket.Message.Receive(c.conn, &msg)
+		fmt.Printf("%v", c.conn.Request().Header)
+		if err != nil {
+			log.Printf("error: %v", err)
+			break
+		}
+		c.server.broadcast <- msg
+	}
+}
+
+func (c *Client) writePump(s *Server) {
+	// NOTE: Pumps messages from *Server to *websocket.Conn.
+	for {
+		select {
+		case msg := <-c.send:
+			// NOTE: added a the key, value pair to the header of the form: Mongo:[true] to identify who the receiver should be.
+			// i.e. the downstream driver client should always send.
+			_ = "breakpoint"
+			for c := range s.clients {
+				if c.conn.Request().Header.Get("Mongo") != "true" {
+					c.conn.Write(msg)
+				}
+			}
+		}
+	}
+}
+
+func (s *Server) run() {
+	for {
+		select {
+		case client := <-s.register:
+			v := randId()
+			s.clients[client] = v
+			log.Printf("client '%s' registered on Server.", v)
+		case message := <-s.broadcast:
+			for c := range s.clients {
+				c.send <- message
+			}
+		}
+	}
+}
+
+var s = newServer()
+
+func webHandler(ws *websocket.Conn) {
+	client := &Client{server: s, conn: ws, send: make(chan []byte)}
+	go s.run()
+	client.server.register <- client
+	go client.writePump(s)
+	client.readPump()
+}
+
+func main() {
+	log.Printf("Listening on :12345..")
+	http.Handle("/ws", websocket.Handler(webHandler))
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
